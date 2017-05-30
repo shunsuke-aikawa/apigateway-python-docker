e164da898e3db1b166cb697481b2bd451a46ff22
syunsuke <shusuke-aikawa@gmotech.jp>
2017-05-30 20:07:33 +0900

    不要な処理削除

--- dev/lambda.py
+++ dev/lambda.py
@@ -3,17 +3,10 @@
 
 def handler(event, context):
 
-    method = event['context']['http-method']
-
     res = {}
-    res['method'] = method
+    res['method'] = vent['context']['http-method']
     res['url'] = event['params']['path']
     res['query-string'] = event['params']['querystring']
     res['json-body'] = event['body-json']
-
-    if method == 'GET':
-        pass
-    else:
-        pass
 
     return res
